bca70ad2c52bb93860e6a99118d504054b3be55a
Juansepo13 <3895@holbertonschool.com>
2021-11-10 09:45:28 -0500

    act all files to the branch master

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Entry point
- * _printf - Function that prints all
+ *
  * Return: Always 0
  */
 int main(void)
@@ -38,12 +38,7 @@ int main(void)
 	len2 = printf("Percent:[%%]\n");
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
-<<<<<<< HEAD
-	_printf("Unknown:[%r]\n");
-/*	printf("Unknown:[%r]\n"); */
-=======
-/*	_printf("Unknown:[%r]\n");*/
-/*	printf("Unknown:[%r]\n");*/
->>>>>>> 7733b7eb635aaef343ce5ad8c94d7e2f3a419331
+	/*_printf("Unknown:[%r]\n");*/
+	/*printf("Unknown:[%r]\n");*/
 	return (0);
 }
--- main.h
+++ main.h
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-#ifndef MAIN_H_
-#define MAIN_H_
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <unistd.h>
-
-/**
- * struct printf_list - Structur
- *@b: indicator
- *@g: Function to call a pointer.
- */
-
-typedef struct printf_list
-{
-	char *b;
-	int (*g)(va_list);
-} pl;
-
-/* Write a function that produces output according to a format */
-int _printf(const char *format, ...);
-int prt_c(va_list);
-int prt_s(va_list);
-int prt_perc(va_list);
-int prt_i(va_list);
-int count_integer(long i);
-int _putchar(char c);
-int _strlen(char *s);
-=======
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
@@ -57,6 +26,5 @@ int printf_s(va_list s);
 int printf_char(va_list c);
 int printf_dec(va_list, char *c_format);
 int print_int(va_list, char *c_format);
->>>>>>> 7733b7eb635aaef343ce5ad8c94d7e2f3a419331
 
 #endif
